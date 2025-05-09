3cfeab01f2483f5605f58dd58cdea2cad02d9105
ARS44PRO <kuimov07@inbox.ru>
2025-05-09 16:00:44 +0700

    Update test and manage.py

--- Python/manage.py
+++ Python/manage.py
@@ -1,6 +1,7 @@
-import board
+from board import SCL,SDA
 from adafruit_pca9685 import PCA9685
-import pigpio
+from adafruit_motor import servo
+import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
@@ -8,35 +9,30 @@ from adafruit_ads1x15.analog_in import AnalogIn
 class Manage():
 
     def __init__(self):
-        self.i2c = board.I2C()
+        self.i2c = busio.I2C(SCL,SDA)
         self.pca = PCA9685(self.i2c)
         self.pca.frequency = 50
-        self.ESC=10 # change to actual pils connects
+        self.my_servo = servo.Servo(self.pca.channels[0])
+        self.esc = servo.Servo(self.pca.channels[1],min_pulse=1000,max_pulse=2000)
         self.ads = ADS.ADS1115(self.i2c)
-        
-        self.pi = pigpio.pi()
-        if not self.pi.connected:
-            print("error")
-            exit()
     
     def mapper(self, x: int, in_min: int, in_max: int, out_min: int, out_max: int)->int:
         return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
-
-    def moving_servo(self, angle: int):
-        angle_after = hex(self.mapper(angle, 0, 180, 0, 0xFFFF))
-        self.pca.channels[0].duty_cycle = angle_after
+    def moving_servo(self,angle: int):
+        self.my_servo.angle = angle
 
 
     def moving_go(self, speed: int):
-        # давать значения будем назад (от -500 до 0), вперед также от 0 до 500
+        # давать значения будем назад (от -255 до 0), вперед также от 0 до 255
         speeding = 0
         if speed<0:
-            speeding = self.mapper(abs(speed), 0, 500, 1500, 500)
+            speeding = self.mapper(abs(speed), 0, 255, 90, 0)
         else:
-            speeding = self.mapper(speed, 0, 500, 1500, 2500)
-        self.pi.set_servo_pulsewidth(self.ESC, speeding)
+            speeding = self.mapper(speed, 0, 255, 90, 180)
+        self.esc.angle = speeding
 
 
     def get_volt(self)->float:
         return AnalogIn(self.ads, ADS.P0).voltage
+    
--- Python/test.py
+++ Python/test.py
@@ -13,12 +13,18 @@ pca.frequency = 50
 
 # Подключаем сервопривод на 0-м канале
 my_servo = servo.Servo(pca.channels[0])
-
+esc = servo.Servo(pca.channels[1],min_pulse=1000,max_pulse=2000)
 # Двигаем сервопривод
 while True:
     my_servo.angle = 0
-    time.sleep(1)
-    my_servo.angle = 90
-    time.sleep(1)
-    my_servo.angle = 180
-    time.sleep(1)
+    esc.angle = 90
+    print(0)
+    time.sleep(2)
+    my_servo.angle = 30
+    esc.angle = 70
+    print(30)
+    time.sleep(2)
+    my_servo.angle = 150
+    esc.angle = 110
+    print(150)
+    time.sleep(2)
